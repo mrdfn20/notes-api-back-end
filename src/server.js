52de5577c0928b71d179e8850859cf7a23310ff3
mrdfn20 <radifan.azman@gmail.com>
2023-06-17 11:41:48 +0800

    add webser plugin with hapi plugin and add validation input with lib joi

--- src/server.js
+++ src/server.js
@@ -1,20 +1,33 @@
 /* eslint-disable quotes */
-const Hapi = require("@hapi/hapi");
-const routes = require("./routes");
+const Hapi = require('@hapi/hapi');
+const notes = require('./api/notes'); //import hapi plugin notes buatan kita
+const Notesservice = require('./services/inMemory/NotesService');
+const NotesValidator = require('./validator/notes'); //import plugin validation
 
 const init = async () => {
+  const notesService = new Notesservice();
+
   const server = Hapi.server({
     port: 5000,
-    host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
-        origin: ["*"],
+        origin: ['*'],
       },
     },
   });
-  server.route(routes);
-  await server.start();
 
+  // assign hapi plugin
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },
+  });
+
+  await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
+
 init();
